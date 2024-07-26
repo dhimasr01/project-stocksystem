521342bb9318f3d55d0bd3c6bd01708f67265711
dhimasr01 <142489351+dhimasr01@users.noreply.github.com>
2024-07-26 07:50:54 +0700

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'PROJECT STOCK - BY JON & WLYDLPZ'
+description 'SYSTEM STOCK | Buy and Sell'
+version '1.0.0'
+
+shared_script 'config.lua'
+
+client_scripts {
+    'client.lua',
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server.lua',
+}
+
+dependencies {
+    'qb-core',
+}
